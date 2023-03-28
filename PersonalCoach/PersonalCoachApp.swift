2edab9a59379d75bc0cbd4e1279e44c4b4e1ae48
lizaboiarchuk <yelyzaveta.boiarchuk@gmail.com>
2023-03-28 16:44:03 +0300

    Initial Commit

---
+++ PersonalCoach/PersonalCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCoachApp.swift
+//  PersonalCoach
+//
+//  Created by Yelyzaveta Boiarchuk on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
