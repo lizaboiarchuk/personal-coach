cea24e9684c85790c87fa0a0052a9131d4c5138d
lizaboiarchuk <yelyzaveta.boiarchuk@gmail.com>
2023-04-28 13:53:47 +0300

    Update WorkoutDetails screen

--- PersonalCoach/Screens/WorkoutDetailsView.swift
+++ PersonalCoach/Screens/WorkoutDetailsView.swift
@@ -7,18 +7,6 @@
 
 import SwiftUI
 
-struct ColoredButtonStyle: ButtonStyle {
-    let color: Color
-    
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .background(color)
-            .clipShape(Circle())
-            .opacity(configuration.isPressed ? 0.7 : 1.0)
-    }
-}
-
-
 
 struct WorkoutDetailsView: View {
     
@@ -50,11 +38,12 @@ struct WorkoutDetailsView: View {
                                         Button(action: {
                                             viewModel.deleteWorkout()
                                         }) {
-                                            Image(systemName: "trash.circle")
+                                            Image(systemName: "trash.circle.fill")
                                                 .font(.system(size: 44))
-                                                .foregroundColor(Color.white)
+                                                .foregroundColor(Color("ColorGrey"))
                                         }
-                                        .buttonStyle(ColoredButtonStyle(color: Color("ColorDarkGreen")))
+                                        .clipShape(Circle())
+                                        .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
                                         .padding(.leading) //: DELETE BUTTON
                                     }
                                     else {
@@ -75,11 +64,12 @@ struct WorkoutDetailsView: View {
                                             Button(action: {
                                                 viewModel.downloadWorkout()
                                             }) {
-                                                Image(systemName: viewModel.currentState == .downloaded ? "checkmark.circle" : "arrow.down.circle")
+                                                Image(systemName: viewModel.currentState == .downloaded ? "checkmark.circle.fill" : "arrow.down.circle.fill")
                                                     .font(.system(size: 44))
-                                                    .foregroundColor(Color.white)
+                                                    .foregroundColor(Color("ColorGrey"))
                                             }
-                                            .buttonStyle(ColoredButtonStyle(color: Color("ColorDarkGreen")))
+                                            .clipShape(Circle())
+                                            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
                                             .disabled(viewModel.currentState == .downloaded || viewModel.currentState == .downloading)
                                             .onChange(of: viewModel.workout.isDownloaded) { newValue in
                                             } //: DOWNLOAD BUTTON
@@ -87,15 +77,14 @@ struct WorkoutDetailsView: View {
                                         Button(action: {
                                             viewModel.navigateToPoseDetection = true
                                         }) {
-                                            Image(systemName: "play.circle")
+                                            Image(systemName: "play.circle.fill")
                                                 .font(.system(size: 44))
-                                                .foregroundColor(Color.white)
+                                                .foregroundColor(Color("ColorGrey"))
                                         }
-                                        .buttonStyle(ColoredButtonStyle(color: Color("ColorDarkGreen")))
+                                        .clipShape(Circle())
+                                        .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
                                         .background(
-                                            NavigationLink(destination: LazyView(PoseDetectionView(workout: viewModel.workout,
-                                                                                                    
-                                                                                                    onDismiss: {
+                                            NavigationLink(destination: LazyView(PoseDetectionView(workout: viewModel.workout, onDismiss: {
                                                 viewModel.navigateToPoseDetection = false
                                                 self.toolbar(.visible, for: .tabBar)
                                             })), isActive: $viewModel.navigateToPoseDetection) {
@@ -113,11 +102,18 @@ struct WorkoutDetailsView: View {
                         
                         Text(viewModel.workout.name)
                             .font(.title)
-                            .fontWeight(.bold)
+                            .fontWeight(.light)
                             .foregroundColor(Color("ColorDarkGreen"))
                         
                         
                         Text("Author: \(viewModel.workout.author)")
+                            .font(.subheadline)
+                            .fontWeight(.light)
+                            .foregroundColor(.gray)
+                            .padding(.bottom, 5)
+                            .foregroundColor(.black)
+                        
+                        Text("Tags: " + viewModel.workout.tags.joined(separator: ", "))
                             .font(.subheadline)
                             .fontWeight(.light)
                             .foregroundColor(.gray)
@@ -128,14 +124,15 @@ struct WorkoutDetailsView: View {
                         VStack {
                             Text("Description")
                                 .font(.body)
-                                .fontWeight(.medium)
+                                .fontWeight(.light)
                                 .padding(.top)
                                 .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, alignment: .leading)
                             Text(viewModel.workout.description)
                                 .font(.body)
                                 .fontWeight(.ultraLight)
                                 .padding(.top, 1)
-                                .multilineTextAlignment(.center)
+                                .multilineTextAlignment(.leading)
                                 .foregroundColor(.black)
                         } //: VSTACK
                     } //: VSTACK
@@ -149,8 +146,27 @@ struct WorkoutDetailsView: View {
             .edgesIgnoringSafeArea(.top)
             .background(Color("ColorGrey")) // Set the background color explicitly
             .navigationBarTitleDisplayMode(.inline)
-//            .navigationBarHidden(true)
+            //            .navigationBarHidden(true)
             
         } //: ZTACK
         .tint(Color("ColorDarkGreen"))    }
+}
+
+
+
+struct WorkoutDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        let wp = WorkoutPreview(workout: Workout(
+            description: "  A light morning workout is the perfect way to kick-start your day and gently awaken your body. This routine focuses on low-impact exercises, incorporating gentle stretches and easy-to-follow movements that target all major muscle groups. Begin with a brief warm-up to increase blood flow and loosen up the muscles. \n\tFollow this with a combination of dynamic stretches, bodyweight exercises, and balance training to improve flexibility and overall body strength. Aim to spend 15-20 minutes on this routine, giving your body the chance to gradually awaken and prepare for the day ahead. Remember to listen to your body, and modify any movements as needed to accommodate your personal fitness level. Finish off your session with a cool-down sequence, including deep breathing exercises and static stretches, to leave you feeling refreshed and energized for the day ahead.",
+            tags: ["morming", "thigs", "light"],
+            cover: "",
+            positions: "",
+            author: "author",
+            video: "",
+            name: "Workoooooout",
+            uid: ""))
+  
+        WorkoutDetailsView(model: LibraryCellViewModel(workout: wp, delegate: LibraryViewModel()))
+    }
 }
